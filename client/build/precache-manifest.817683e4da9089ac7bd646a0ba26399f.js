bee7ca0f530be0bff988d271ade09ff4d9b6da4c
junyyulk18 <trangsam18@gmail.com>
2022-12-13 08:52:07 +0700

    add client
    ..

---
+++ client/build/precache-manifest.817683e4da9089ac7bd646a0ba26399f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2824a8db02045b9adf181500614eb78f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f10d8e3bec6a60069ea",
+    "url": "/static/css/2.e0357e7c.chunk.css"
+  },
+  {
+    "revision": "ffdae1a18037223ba241",
+    "url": "/static/css/main.cd61c0e5.chunk.css"
+  },
+  {
+    "revision": "4f10d8e3bec6a60069ea",
+    "url": "/static/js/2.da78d6b9.chunk.js"
+  },
+  {
+    "revision": "b0d61e8c7db48801156d2e3b41dc909b",
+    "url": "/static/js/2.da78d6b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffdae1a18037223ba241",
+    "url": "/static/js/main.70f03325.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.70f03325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
