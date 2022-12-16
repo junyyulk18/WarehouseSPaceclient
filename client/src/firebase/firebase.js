74e552957e14f81c11661c7095332d83b9a1ab60
junyyulk18 <trangsam18@gmail.com>
2022-12-16 18:14:48 +0700

    Update firebase.js

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -3,14 +3,15 @@ import 'firebase/storage';
 import 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdHRD1CizYsWb9RWG3kk-DYPf0RPvzDrg",
-  authDomain: "shoponline-fd329.firebaseapp.com",
-  projectId: "shoponline-fd329",
-  storageBucket: "shoponline-fd329.appspot.com",
-  messagingSenderId: "101598190960",
-  appId: "1:101598190960:web:50560415eab000d8e443b5",
-  measurementId: "G-BP816K62PC"
-};
+  apiKey: "AIzaSyAn0hB0skriGqd4WxE7jpFv1ibeNaF21mI",
+  authDomain: "warehousespace-36258.firebaseapp.com",
+  projectId: "warehousespace-36258",
+  storageBucket: "warehousespace-36258.appspot.com",
+  messagingSenderId: "157925804181",
+  appId: "1:157925804181:web:61b2669b95ca0a1e4af39c",
+  measurementId: "G-MK9P5QGW28"
+}; 
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
